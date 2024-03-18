93aef651bc3ec3cf8e3de19b1206546368ea1029
Matt Mullen <mhmullen@icloud.com>
2024-03-18 11:58:57 -0700

    added nav bar verbiage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './assets/components/Nav'
+// import List from './components/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <Nav />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,35 @@
+export default function Nav() {
+    const linkStyle = { border: '1px black', padding: '5px', margin: '5px' };
+
+    return (
+        <nav className="main-header-menu">
+            <section
+                style ={{
+                    display: 'flex',
+                    fontFamily: 'sans-serif',
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    justifyContent: 'flex-start',
+                }}
+            >
+                <div style={linkStyle}>
+                    <a href="#">About Me</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Portfolio</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Contact</a>
+                </div>
+                <div style={linkStyle}>
+                    <a href="#">Resume</a>
+                </div>
+            </section>
+        </nav>
+    );
+}
+
+               
+
+
+        
