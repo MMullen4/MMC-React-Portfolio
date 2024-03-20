1231ba09b727739bc9b185d1c858e598c399710a
Matt Mullen <mhmullen@icloud.com>
2024-03-20 16:17:50 -0700

    revised About Me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,11 +3,16 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <card>
-        <h1>Matt Mullen - Committed to Integrity & Excellence</h1>
-        <p>With over 20 years in Paperboard manufacturing & sales to a wide array of printers, packagers, and various converters of paperboard, Matt brings industry experience and knowledge together to find solutions for clients that satisfy their needs to expand and penetrate their target markets.
+        <h1>Matt Mullen</h1>
+        <h2>Owner</h2>
+        <img src=".assets/images/MM.png" alt="Matt Mullen" />
+        
+        <p> With over 20 years in Paperboard manufacturing & sales to a wide array of printers, packagers, and various converters of paperboard, Matt brings industry experience and knowledge together to find solutions for clients that satisfy their needs to expand and penetrate their target markets.
           In his spare time, Matt enjoys the outdoors with his family.  He also volunteers as a Court Appointed Special Advocate (CASA) in LA County, whose goals are to help children subjected to neglect and abuse.
         </p>
       </card>
     </div>
   );
-}
+
+
+} 
